6bbaafaeaf8fa9d06f2f2724bc855fbe571059e0
tiago-macedo <macedo.tiaho@yahoo.com.br>
2020-12-21 09:34:11 -0300

    Parte sequencial.

--- lab3/main.c
+++ lab3/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <math.h>
 
 #include "timer.h"	// para cronometrar partes do programa
 #include "safety.h"	// para auto-verificar retorno de NULL em malloc e calloc
@@ -15,27 +16,43 @@ void help(char*);	// exibe mensagem de ajuda
 
 // MAIN
 int main(int argc, char* argv[]) {
-	// Checagem dos parâmetros de linha de comando
+	double start, end;
+	
+	// Checagem e carregamento dos parâmetros de linha de comando
 	if (argc < 3) {
 		help(argv[0]);
 		return 1;
 	}
 	unsigned long long int N = strtoull(argv[1], NULL, 10);	// converte char* para unsigned long long int
 	int n_threads = atoi(argv[2]);
-
+	
 	// Declaração e inicialização de estruturas de dados
 	
 	// Carregamento de estruturas com dados do usuário
 	
 	// Conta sequencial
-	
+	long double sumSeq = 0;
+	for (long double n=1.; n <= N; n++) {
+		sumSeq += 4./(2.*n - 1.);
+		sumSeq *= -1;
+	}
+	if (N % 2 == 1)
+		sumSeq *= -1;
 	// Conta concorrente
 	
 	// 1: Declarar threads
+	/*
+	unsigned long long int* ids = malloc(n_threads * sizeof(unsigned long long int));
+	pthread_t* threads = malloc(n_threads * sizeof(pthread_t));
+	*/
 	// 2: Disparar threads
 	// 3: Capturar threads
 	
 	// Exibindo resultados
+	printf(	"math.h:	π = %.15lf\n"
+			"sumSeq:	π = %.15Lf\n",
+			M_PI,
+			sumSeq);
 	return 0;
 }
 
